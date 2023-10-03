b75fb1d05fdb642b8c4135e4610bcf3c433e515f
dilancroos <dilancroos@outlook.com>
2023-10-03 23:48:02 +0200

    Update - simple -> Caplitalized

--- backEnd/utils.py
+++ backEnd/utils.py
@@ -1,4 +1,4 @@
-from .constants import calories, combos
+from .constants import calories, combos, meals, combos2
 
 
 def calories_counter(*args):
@@ -7,10 +7,11 @@ def calories_counter(*args):
 
     for item in args:
         try:
+            item = item.capitalize()
             if item in calories:
                 total_calories += calories[item]
             else:
-                for combo_item in combos[item]:
+                for combo_item in combos[item.capitalize()]:
                     total_calories += calories[combo_item]
 
         except KeyError:
