dc5214945ff69454698c1a4a84627df125b1c4e3
dilancroos <dilancroos@outlook.com>
2023-10-30 23:22:51 +0100

    added JSON unit tests

--- test/test_functions.py
+++ test/test_functions.py
@@ -1,8 +1,7 @@
 from unittest import TestCase
 
 from backEnd.utils import calories_counter, comp_cal_counter, comp_cal_counter2, price_counter
-
-import backEnd.classes as classes
+from backEnd.jsonUtils import comp_cal_counter_json, price_counter_json
 
 class calories_counterTestCase(TestCase):
     def test_calaries_simple(self):
@@ -129,4 +128,55 @@ class price_counterTestCase(TestCase):
 
     def test_price_complex_name_unknown(self):
         result = price_counter('hamburger','salad','iced tea', 'Unknown')
+        self.assertEqual(result, 11, f"Expected 11, got {result}")
+
+class json_fileTestCase(TestCase):
+
+    def test_json_calaries_combo_complex_id(self):
+        result = comp_cal_counter_json('combo-1', 'combo-2')
+        self.assertEqual(result, 1770, f"Expected 1770, got {result}")
+
+    def test_json_calaries_combo_complex_id_unknown(self):
+        result = comp_cal_counter_json('combo-1', 'combo-2', 'Unknown')
+        self.assertEqual(result, 1770, f"Expected 1770, got {result}")
+
+    def test_json_calaries_combo_complex_name(self):
+        result = comp_cal_counter_json('cheesy combo', 'veggie combo')
+        self.assertEqual(result, 1770, f"Expected 1770, got {result}")
+
+    def test_json_calaries_combo_complex_name_unknown(self):
+        result = comp_cal_counter_json('cheesy combo', 'veggie combo', 'Unknown')
+        self.assertEqual(result, 1770, f"Expected 1770, got {result}")
+
+    def test_json_calaries_complex_mix_id(self):
+        result = comp_cal_counter_json('meal-1', 'combo-2')
+        self.assertEqual(result, 1300, f"Expected 1300, got {result}")
+
+    def test_json_calaries_complex_mix_id_unknown(self):
+        result = comp_cal_counter_json('meal-1', 'combo-2', 'Unknown')
+        self.assertEqual(result, 1300, f"Expected 1300, got {result}")
+
+    def test_json_calaries_complex_mix_name(self):
+        result = comp_cal_counter_json('hamburger', 'veggie combo')
+        self.assertEqual(result, 1300, f"Expected 1300, got {result}")
+
+    def test_json_calaries_complex_mix_name_unknown(self):
+        result = comp_cal_counter_json('hamburger', 'veggie combo', 'Unknown')
+        self.assertEqual(result, 1300, f"Expected 1300, got {result}")
+    
+
+    def test_json_price_complex_id(self):
+        result = price_counter_json('meal-1', 'meal-2', 'meal-3')
+        self.assertEqual(result, 18, f"Expected 18, got {result}")
+
+    def test_json_price_complex_id_unknown(self):
+        result = price_counter_json('meal-1', 'meal-2', 'meal-3', 'Unknown')
+        self.assertEqual(result, 18, f"Expected 18, got {result}")
+
+    def test_json_price_complex_name(self):
+        result = price_counter_json('hamburger','salad','iced tea')
+        self.assertEqual(result, 11, f"Expected 11, got {result}")
+
+    def test_json_price_complex_name_unknown(self):
+        result = price_counter_json('hamburger','salad','iced tea', 'Unknown')
         self.assertEqual(result, 11, f"Expected 11, got {result}")
