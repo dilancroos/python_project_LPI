74b5125cf8f897d5a51d92c6978ff00bcef04686
dilancroos <dilancroos@outlook.com>
2023-10-31 09:36:49 +0100

    unittest updates

--- tests/test_classes.py
+++ tests/test_classes.py
@@ -1,7 +1,7 @@
 from unittest import TestCase
 from backEnd.classes import Order
 
-class OrderMakingTestCase(TestCase):
+class orderMakingTestCase(TestCase):
     def check_order_counter(self):
         current_counter = Order.counter
         order1 = Order([])
@@ -9,9 +9,3 @@ class OrderMakingTestCase(TestCase):
         order2 = Order([])
         self.assertEqual(order2.order_id, f"order-{current_counter + 1 }")
          
-
-
-# class CalCounterTestCase(TestCase):
-#     def individual_items(self):
-#         oneItem = ['Hamburger']
-#         assert calories_counter(oneItem) == '600' 
